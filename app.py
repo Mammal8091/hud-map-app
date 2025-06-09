c10e888a7149eab7685cd9fd821e57b84c83641e
Mammal8091 <gleb@hvpg.com>
2025-06-09 17:21:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 
 # Load HUD property data
-df = pd.read_csv("hud_properties.csv")
+df = pd.read_csv("hud_properties.csv", encoding="utf-8", errors="replace")
 
 # Streamlit layout
 st.set_page_config(layout="wide")
