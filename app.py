122ba9c7177e86c353b3122e24343d1489bef05a
Mammal8091 <gleb@hvpg.com>
2025-06-09 17:28:50 -0400

    Update app.py to encoding="ISO-8859-1"

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 
 # Load HUD property data
-df = pd.read_csv("hud_properties.csv", encoding="utf-8", errors="replace")
+df = pd.read_csv("hud_properties.csv", encoding="ISO-8859-1", errors="replace")
+
 
 # Streamlit layout
 st.set_page_config(layout="wide")
